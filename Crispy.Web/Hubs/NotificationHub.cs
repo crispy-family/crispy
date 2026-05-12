56c6cc186c68a55548537d4573e51865a72eea95
artemtipa228@gmail.com <artemtipa228@gmail.com>
2026-05-12 23:00:23 +0300

    delete comments

--- Crispy.Web/Hubs/NotificationHub.cs
+++ Crispy.Web/Hubs/NotificationHub.cs
@@ -4,8 +4,6 @@ namespace Crispy.Web.Hubs
 {
     public class NotificationHub : Hub
     {
-        // Поки що нам не потрібно додавати кастомні методи сюди,
-        // оскільки ми будемо відправляти повідомлення з BackgroundService
-        // до конкретних користувачів за їхнім UserIdentifier (зазвичай це ClaimTypes.NameIdentifier).
+        
     }
 }
